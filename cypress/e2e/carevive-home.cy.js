5d5bcf99afc44ebc388ca17a9068e489925dc0ea
op-gill-twistle <opinderpal.gill@HCAop-macbook.local>
2025-07-23 23:47:09 -0700

    Fix login and homepage test flow; update support file

--- cypress/e2e/carevive-home.cy.js
+++ cypress/e2e/carevive-home.cy.js
@@ -2,12 +2,13 @@ import { loginPage, homePage } from './loginPage'
 
 describe('Carevive Home Page', () => {
   it('logs in with secure credentials and verifies homepage', () => {
-    loginPage.visit()
-    loginPage.enterUsername(Cypress.env('USERNAME'))
-    loginPage.enterPassword(Cypress.env('PASSWORD'))
-    loginPage.clickLogin()
-    const appOrigin = Cypress.config('baseUrl').replace(/\/$/, '')
     cy.fixture('selectors').then((selectors) => {
+      cy.visit('/')
+      cy.get(selectors.usernameInput).should('be.visible').type(Cypress.env('USERNAME'))
+      cy.get(selectors.passwordInput).should('be.visible').type(Cypress.env('PASSWORD'))
+      cy.get(selectors.loginButton).click()
+      // Verify homepage on app domain
+      const appOrigin = Cypress.config('baseUrl').replace(/\/$/, '')
       cy.origin(appOrigin, { args: { logoImage: selectors.logoImage } }, ({ logoImage }) => {
         cy.get(logoImage).should('be.visible')
       })
